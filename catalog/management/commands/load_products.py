d406e8f7aead1291bb1232012dace8f28c068692
Vladimir <antonovvaldemar5@gmail.com>
2026-03-26 19:52:03 +0300

    Исправлены кастомные команды

--- catalog/management/commands/load_products.py
+++ catalog/management/commands/load_products.py
@@ -1,32 +1,18 @@
 import json
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Category, Product
 
 class Command(BaseCommand):
-    help = "Загружает тестовые продукты из JSON-фикстур"
+    help = "Загружает тестовые данные из фикстур"
 
     def handle(self, *args, **options):
-
+        # Очищаем данные
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-
-        with open('categories.json', 'r', encoding='utf-8') as f:
-            categories_data = json.load(f)
-            for cat_data in categories_data:
-                Category.objects.create(**cat_data['fields'])
-
-
-        with open('products.json', 'r', encoding='utf-8') as f:
-            products_data = json.load(f)
-            for prod_data in products_data:
-                # Находим категорию по id
-                category_id = prod_data['fields']['category']
-                category = Category.objects.get(id=category_id)
-                Product.objects.create(
-                    name=prod_data['fields']['name'],
-                    price=prod_data['fields']['price'],
-                    category=category
-                )
+        # Загружаем фикстуры
+        call_command('loaddata', 'categories.json')
+        call_command('loaddata', 'products.json')
 
         self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
