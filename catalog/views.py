5894b660d74e3bca3b08f39942397efa33effa30
Vladimir <antonovvaldemar5@gmail.com>
2026-04-08 19:17:18 +0300

    Обновлены контроллеры

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+
 from .models import Product
 
+class HomeListView(ListView):
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
 
-def home(request):
-    products = Product.objects.all()
-    return render(request, "catalog/home.html", {'products': products})
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    return render(request, "catalog/product_detail.html", {'product': product})
+
+class ContactsTemplateView(TemplateView):
+    model = Product
+    template_name = "catalog/contacts.html"
+
+
 
