f5d38b8c32fa2c78535ca12400412cbb60a312cd
Vladimir <antonovvaldemar5@gmail.com>
2026-04-08 21:41:18 +0300

    исправил catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ app_name = "catalog"
 
 urlpatterns = [
     path('', views.HomeListView.as_view(), name='home'),
-    path('contacts/', views.TemplateView.as_view(), name='contacts'),
-    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail')
+    path('contacts/', views.ContactsTemplateView.as_view(), name='contacts'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
